24308e9b0548e411a1e96479f492d817893f378f
Xavier Redó <xavier.redo@marsbased.com>
2017-03-15 12:01:48 +0100

    Add Vilanova seeds (#15)
    Includes logos, images, static pages and organization info.
    Closes #10

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,58 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#Decidim.seed! if Rails.env.development?
 
-# You can remove the 'faker' gem if you don't want Decidim seeds.
-Decidim.seed!
+organization = Decidim::Organization.first || Decidim::Organization.new
+organization.scopes.destroy_all
+
+organization.available_locales = [:ca, :es]
+organization.default_locale = :ca
+organization.host = ENV['APP_HOST'] || 'localhost'
+organization.name = 'Participa Vilanova'
+organization.official_url = 'https://www.vilanova.cat'
+organization.reference_prefix = 'VG'
+
+organization.welcome_text = {
+  ca: "Construïm entre tots i totes la ciutat que volem",
+  es: "Construyamos entre todos y todas la ciudad que queremos"
+}
+organization.description = {
+  ca: "Benvingut i benvinguda a la web de participació ciutadana de Vilanova i la Geltrú. A través d’aquest espai podràs obrir debats, opinar, fer propostes i decidir sobre el temes de ciutat que t’interessen.",
+  es: "Bienvenidos y bienvenidas a la web de participación ciudadana de Vilanova i la Geltrú. A través de este espacio podrás abrir debates, opinar, hacer propuestas y decidir sobre los temas de la ciudad que te interesan."
+}
+
+organization.twitter_handler = 'ajuntamentvng'
+organization.facebook_handler = 'ajuntamentvng'
+organization.instagram_handler = 'ajuntamentdevilanovailageltru'
+organization.youtube_handler = 'devilanovailageltru'
+organization.github_handler = 'vilanovailageltru'
+
+organization.logo = File.new(Rails.root.join('db', 'seeds', 'vilanova-logo-white.png'))
+organization.official_img_footer = File.new(Rails.root.join('db', 'seeds', 'vilanova-logo-white-footer.png'))
+organization.official_img_header = File.new(Rails.root.join('db', 'seeds', 'vilanova-badge.png'))
+organization.homepage_image = File.new(Rails.root.join('db', 'seeds', 'vilanova-hero-home.jpg'))
+organization.favicon = File.new(Rails.root.join('db', 'seeds', 'favicon.png'))
+
+organization.save!
+
+organization.static_pages.find_or_initialize_by(slug: 'faq').update(
+  title: { ca: 'Què és Participa Vilanova i la Geltrú?', es: '¿Qué es Participa Vilanova i la Geltrú?' },
+  content: {
+    ca: File.new(Rails.root.join('db', 'seeds', 'faq.ca.html')).read,
+    es: File.new(Rails.root.join('db', 'seeds', 'faq.es.html')).read
+  }
+)
+
+organization.static_pages.find_or_initialize_by(slug: 'terms-and-conditions').update(
+  title: { ca: "Termes i condicions d'ús", es: 'Términos y condiciones de uso' },
+  content: {
+    ca: File.new(Rails.root.join('db', 'seeds', 'terms.ca.html')).read,
+    es: File.new(Rails.root.join('db', 'seeds', 'terms.es.html')).read
+  }
+)
+
+organization.static_pages.find_or_initialize_by(slug: 'accessibility').update(
+  title: { ca: 'Accessibilitat', es: 'Accesibilidad' },
+  content: {
+    ca: File.new(Rails.root.join('db', 'seeds', 'accessibility.ca.html')).read,
+    es: File.new(Rails.root.join('db', 'seeds', 'accessibility.es.html')).read
+  }
+)

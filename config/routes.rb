fb03ec70c8267c16f8d0a1520c9ff6dd191f0932
Xavier Redó <xavier.redo@marsbased.com>
2018-11-02 16:29:30 +0100

    Force Dockerfile Ruby version to 2.4.0
    Add letter opener web routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  if Rails.application.config.action_mailer.delivery_method == :letter_opener_web
+    mount LetterOpenerWeb::Engine, at: '/letter_opener'
+  end
+
   mount Decidim::Core::Engine => '/'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end

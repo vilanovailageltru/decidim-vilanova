90df1d674e6c826c0652826d3313814168bac0fe
Xavier Redó <xavier.redo@marsbased.com>
2018-11-05 11:08:20 +0100

    Change default Uglifier for harmony one to support ES6

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
 
   config.public_file_server.enabled = false
 
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   config.assets.compile = false
 
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect'

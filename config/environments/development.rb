15c265c020e73766247bf4e0fd419ceb7636f1e4
Fernando Blat <fernando@blat.es>
2022-05-31 16:07:13 +0200

    Mac dev env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,7 +50,9 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  # Use an evented file watcher to asynchronously detect changes in source code,
-  # routes, locales, etc. This feature depends on the listen gem.
-  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  if RUBY_PLATFORM !~ /arm64.*darwin/ || `sysctl -n machdep.cpu.brand_string` !~ /M1/
+    # Use an evented file watcher to asynchronously detect changes in source code,
+    # routes, locales, etc. This feature depends on the listen gem.
+    config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  end
 end
